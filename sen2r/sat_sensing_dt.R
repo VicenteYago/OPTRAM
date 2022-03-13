e85c9f7d3803ec47cdfab72c9d65a8e7de3e1e79
Yago <josevicente.yago@um.es>
2022-03-13 23:49:19 +0100

    retrieving sensing datetime ok

--- sen2r/sat_sensing_dt.R
+++ sen2r/sat_sensing_dt.R
@@ -1,17 +1,18 @@
-# Rscript sat_sensing_dt "2021-01-01" "2021-01-10" Walnut-Gulch.geojson
+# Rscript sat_sensing_dt.R "2021-01-01" "2021-01-10" Walnut-Gulch.geojson
 
 library(magrittr)
 
 args <- commandArgs(trailingOnly = TRUE)
-geojson  = args[0]
+print(args)
 start_dt = args[1] # yyyy-mm-dd
 end_dt   = args[2]
+geojson  = args[3]
 
 #files <- list.files(folder.BOA, full.names = T)
-
 time_window <- as.Date(c(start_dt, end_dt))
-results <- geojsonsf::geojson_sfc(parcela.json) %>% 
-  sen2r::s2_list(time_interval = time_window)
+print(geojson)
+gj.sf = geojsonsf::geojson_sfc(geojson)
+results = sen2r::s2_list(gj.sf, time_interval = time_window)
 
 sensing_dt <- sen2r::safe_getMetadata(results, "sensing_datetime")
 sensing_dt
