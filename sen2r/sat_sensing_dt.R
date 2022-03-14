af2e67b9fd54732bb6818545fd1dfc25d5cc04f7
Yago <josevicente.yago@um.es>
2022-03-14 17:19:58 +0100

    sat_sensing_dt OK + viz + comments

--- sen2r/sat_sensing_dt.R
+++ sen2r/sat_sensing_dt.R
@@ -1,16 +1,11 @@
-# Rscript sat_sensing_dt.R "2021-01-01" "2021-01-10" Walnut-Gulch.geojson
-
-library(magrittr)
+# Rscript sat_sensing_dt.R "2021-01-01" "2021-01-10" Walnut-Gulch.geojson 2> /dev/null
 
 args <- commandArgs(trailingOnly = TRUE)
-print(args)
 start_dt = args[1] # yyyy-mm-dd
-end_dt   = args[2]
+end_dt   = args[2] # yyyy-mm-dd
 geojson  = args[3]
 
-#files <- list.files(folder.BOA, full.names = T)
 time_window <- as.Date(c(start_dt, end_dt))
-print(geojson)
 gj.sf = geojsonsf::geojson_sfc(geojson)
 results = sen2r::s2_list(gj.sf, time_interval = time_window)
 
